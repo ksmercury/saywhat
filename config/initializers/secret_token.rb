2ffc865c572d4c9e45a71371dff59c7be1970cc8
unknown <kim@.(none)>
2011-03-26 15:47:56 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saywhat::Application.config.secret_token = '809d2fc8c7f11b0e198c5ea151b8e3ed9939b76a31d144cf658e86d76b81b69a398807f8f369194e13133d0a3d5755e85e14c8c32f033d1c9abd3ee2275e04dd'
